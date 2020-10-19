0c93e091edcf00d04672fd0b9a3cbc4ca858a16c
Bradley Taylor <bradley.taylor07@gmail.com>
2020-10-19 22:08:01 +0100

    Complete comments

--- src/com/bradleytaylor/Main.java
+++ src/com/bradleytaylor/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Single line comment
+        /*
+            This
+            is
+            a
+            multi
+            line
+            comment.
+         */
         System.out.println("First Java App");
 
     }
