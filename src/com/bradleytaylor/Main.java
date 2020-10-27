fa73bfff95c8f6e0ee6083b9e8ec9654440fadbe
Bradley Taylor <bradley.taylor07@gmail.com>
2020-10-27 14:05:41 +0000

    Complete difference between primitive and reference types

--- src/com/bradleytaylor/Main.java
+++ src/com/bradleytaylor/Main.java
@@ -6,10 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String name = new String("Bradley Taylor");
-        LocalDate today = LocalDate.now();
-        System.out.println(name.toUpperCase());
-        System.out.println(today.getYear());
+        int a = 10;
+        int b = a;
+        a = 100;
+        System.out.println("a = " + a + " and b = " + b);
 
+        Person Bradley = new Person("Bradley");
+        Person Taylor = Bradley;
+
+        System.out.println("Before");
+        System.out.println(Bradley.name + " " +Taylor.name);
+
+        Bradley.name = "Brad";
+
+        System.out.println("After");
+        System.out.println(Bradley.name + " " +Taylor.name);
+    }
+
+    static class Person {
+
+        String name;
+
+        Person(String name) {
+            this.name = name;
+        }
     }
 }
