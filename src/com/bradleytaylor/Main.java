cda6f1b470ac73be64623d3ce0edbcd664b9b50f
Bradley Taylor <bradley.taylor07@gmail.com>
2020-10-27 21:19:13 +0000

    Complete ternary operator

--- src/com/bradleytaylor/Main.java
+++ src/com/bradleytaylor/Main.java
@@ -4,15 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int age = 22;
+        int age = 19;
+        String message = age >= 20 ? "Adult" : "Child";
 
-        if (age >= 20) {
-            System.out.println("Adult");
-        } else if (age >= 13 && age <= 19){
-            System.out.println("Teenager");
-        } else {
-            System.out.println("Child");
-        }
+        System.out.println(message);
 
     }
 }
