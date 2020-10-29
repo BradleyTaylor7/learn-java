6f95a94176a0a6bbfd1ecb5bb915eeb8f9e31e84
Bradley Taylor <bradley.taylor07@gmail.com>
2020-10-29 18:25:42 +0000

    Complete while loop

--- src/com/bradleytaylor/Main.java
+++ src/com/bradleytaylor/Main.java
@@ -4,20 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] names = {"Bradley", "William", "Taylor"};
+        int counter = 0;
 
-        for (String name : names) {
-            if (name.equals("Taylor")) {
-                break;
-            }
-            System.out.println(name);
-        }
-
-        for (String name : names) {
-            if (name.startsWith("B")) {
-                continue;
-            }
-            System.out.println(name);
+        while (counter <= 10) {
+            System.out.println(counter);
+            counter++;
         }
     }
 }
