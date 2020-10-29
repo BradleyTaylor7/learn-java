92bb3d7b3a760644b3d0ae9a54b4d9df1719cf93
Bradley Taylor <bradley.taylor07@gmail.com>
2020-10-29 18:17:58 +0000

    Complete break and continue

--- src/com/bradleytaylor/Main.java
+++ src/com/bradleytaylor/Main.java
@@ -4,21 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = {20, 14, 39, 42, 18};
-        String[] names = {"Bradley", "Taylor"};
+        String[] names = {"Bradley", "William", "Taylor"};
 
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-
-        // enhanced for loop
-        System.out.println("\nEnhanced for loop");
-        for (int number : numbers) {
-            System.out.println(number);
-        }
-
-        System.out.printf("%n");
         for (String name : names) {
+            if (name.equals("Taylor")) {
+                break;
+            }
+            System.out.println(name);
+        }
+
+        for (String name : names) {
+            if (name.startsWith("B")) {
+                continue;
+            }
             System.out.println(name);
         }
     }
