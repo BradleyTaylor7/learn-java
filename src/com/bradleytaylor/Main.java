ca80eebae46cc37c078d22b0b0639144489ea303
Bradley Taylor <bradley.taylor07@gmail.com>
2020-10-29 12:22:08 +0000

    Complete arrays and indexes

--- src/com/bradleytaylor/Main.java
+++ src/com/bradleytaylor/Main.java
@@ -6,23 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String [] name = {"Bradley", "Taylor"};
+        int [] numbers = {1, 2, 5, 4, 3};
 
-        // 1 line to create an array and add values
-        int [] numbers = {1, 2};
-
-        // longer way to create an array and add values
-        int [] numbersOther = new int[2];
-
-        numbersOther[0] = 1;
-        numbersOther[1] = 2;
-
-        System.out.println(Arrays.toString(name));
-        System.out.println(name.length);
-        System.out.println(Arrays.toString(numbers));
-        System.out.println(numbers.length);
-        System.out.println(Arrays.toString(numbersOther));
-        System.out.println(numbersOther.length);
-
+        int four = numbers[3];
+        int three = numbers[numbers.length - 1];
+        System.out.println(four);
+        System.out.println(three);
     }
 }
